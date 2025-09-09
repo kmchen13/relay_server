ba27cbc6adea9668680ce0bf5295abc4cdc02733
kmchen <webologix@gmail.com>
2025-09-09 03:10:11 +0200

    1.3.13

--- bin/handlers/disconnect_handler.dart
+++ bin/handlers/disconnect_handler.dart
@@ -1,12 +1,23 @@
 import 'dart:io';
 import '../utils/player_utils.dart';
 import '../utils/json_utils.dart';
-import '../constants.dart';
 
 Future<void> handleDisconnect(HttpRequest req) async {
-  if (debug) {
-    print(
-        "[$appName v$version] ❌ /disconnect should not be called in web mode");
-    showPlayers();
+  final userName = req.uri.queryParameters['userName'] ?? '';
+
+  if (userName == '') {
+    req.response.statusCode = HttpStatus.badRequest;
+    return jsonResponse(req.response, {
+      'error': 'missing_parameters',
+      'message': 'userName et expectedName sont requis',
+    });
   }
+  players.removeWhere((p) => p.userName == userName && p.expectedName == '');
+  savePlayers();
+
+  req.response.statusCode = HttpStatus.ok;
+  return jsonResponse(req.response, {
+    'status': 'disconnected',
+    'message': '$userName déconnecté avec succès',
+  });
 }
